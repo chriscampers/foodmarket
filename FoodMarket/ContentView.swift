629d2d950ea74a694385b60c7bd94ebe28a00732
Chris Campanelli <chris.campanelli@raceroster.com>
2021-06-18 15:34:49 -0400

    Initial Commit

---
+++ FoodMarket/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FoodMarket
+//
+//  Created by Chris Campanelli on 2021-06-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FoodMarket/FoodMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodMarketApp.swift
+//  FoodMarket
+//
+//  Created by Chris Campanelli on 2021-06-18.
+//
+
+import SwiftUI
+
+@main
+struct FoodMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
